4b4d5c373ad6d173143111c052ea3bc3d03018c5
Donjella <brendonang@gmail.com>
2024-09-18 01:52:30 +1000

    Initial setup for classes

---
+++ classes/parking_slot.py
@@ -0,0 +1,11 @@
+class ParkingSlot:
+    def __init__(self, id):
+        self.id = id
+        self.car = None
+
+    def __str__(self):
+        #  if self.car: - means if self.car is present, i.e. a car is present or self.car != none
+        if self.car:
+            return f"Parking slot with id {self.id} and has the following car: {self.car}"
+        else:
+            return f"Parking slot with id {self.id}"
